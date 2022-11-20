b007dd9acb2e90851dd1d27b43aa759bb0cc4ce9
Noru <akang8150@naver.com>
2022-11-20 03:01:08 +0000

    15.6 Environment Files

--- config/settings.py
+++ config/settings.py
@@ -11,22 +11,27 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
+
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ch4^&1og*8u&irs+4911m^m44!e7em+(%4x@!$qcsovtft&_i&'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ["localhost"]
-CSRF_TRUSTED_ORIGINS = ["https://*.ws-us75.gitpod.io"]
+CSRF_TRUSTED_ORIGINS = [env("GITPOD_SERVER_DOMAIN")]
 
 
 # Application definition
